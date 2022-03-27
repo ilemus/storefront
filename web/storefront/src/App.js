61b37fa1baa1ac4e5af5205bae9d52af03139242
ilemus <ilemus@umich.edu>
2022-03-27 15:32:49 -0500

    react app

---
+++ web/storefront/src/App.js
@@ -0,0 +1,20 @@
+import { CssBaseline } from '@mui/material';
+import './App.css';
+import PrimarySearchAppBar from './components/Titlebar';
+
+function App() {
+  return (
+    <div>
+    <CssBaseline />
+    <PrimarySearchAppBar />
+    <div>
+      <p>
+        Here is my content.<br />
+        Here is more of it.
+      </p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
